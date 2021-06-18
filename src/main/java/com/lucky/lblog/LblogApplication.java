3b7ef126bab5146be7cc5acfafb0ccce9764ede9
姜运峰 <3170105375@zju.edu.cn>
2021-06-18 15:23:55 +0800

    '项目初始化'

---
+++ src/main/java/com/lucky/lblog/LblogApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.lblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucky/lblog/LblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.lblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
